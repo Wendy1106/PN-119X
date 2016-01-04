bd1ecf1192f18f06520ce78de3fe2c504f4f7553
Danza <412027359@qq.com>
2016-01-04 13:58:11 +0800

    Create fw.info.h

---
+++ fw.info.h
@@ -0,0 +1,15 @@
+typedef enum {
+        BOOT_CONSOLE_MODE = 0,
+        BOOT_RESCUE_MODE,
+        BOOT_NORMAL_MODE,
+        BOOT_MANUAL_MODE,
+        BOOT_QC_VERIFY_MODE,
+        BOOT_FLASH_WRITER_MODE,
+        /regist new method
+        BOOT_TFTP_KERN_MODE,
+        //regist new method
+        BOOT_TFTP_UBOT_MODE,
+        BOOT_MODE_NUM,  
+        BOOT_ANDROID_MODE,      
+		BOOT_KERNEL_ONLY_MODE	
+} BOOT_MODE;
